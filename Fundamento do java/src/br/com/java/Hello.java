bf5c7d3bc341b1ac2fbaf3c2fccbab1a0b41fd60
odeh12 <38111118+odeh12@users.noreply.github.com>
2018-05-21 17:02:34 -0300

    Add files via upload

---
+++ Fundamento do java/src/br/com/java/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.java;
+
+/**
+ * @author bello.puwadiae
+ * Hello World
+ * House
+ *
+ */
+public class Hello {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}
+
+}
